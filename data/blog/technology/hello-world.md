c6b5169b9a0d38f190805a19a82cc5352f2fe1f8
Liam Sorsby <liam.sorsby@sky.com>
2023-01-02 20:44:19 +0000

    initial commit

---
+++ data/blog/technology/hello-world.md
@@ -0,0 +1,11 @@
+---
+title: Hello World
+date: '2023-01-02'
+tags: ['technology']
+draft: false
+summary: 'A new year, A new website.'
+---
+
+# Hello World
+
+It's about time that I started a blog / website. So hear it is!
--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -1,22 +1,22 @@
 const siteMetadata = {
-  title: 'Next.js Starter Blog',
-  author: 'Tails Azimuth',
-  headerTitle: 'TailwindBlog',
-  description: 'A blog created with Next.js and Tailwind.css',
-  language: 'en-us',
+  title: 'Liam Sorsby: Blog',
+  author: 'Liam Sorsby',
+  headerTitle: 'Liam Sorsby',
+  description: 'Liam Sorsby: asdasd',
+  language: 'en-gb',
   theme: 'system', // system, dark or light
-  siteUrl: 'https://tailwind-nextjs-starter-blog.vercel.app',
-  siteRepo: 'https://github.com/timlrx/tailwind-nextjs-starter-blog',
+  siteUrl: 'https://sorsby.co',
+  siteRepo: 'https://github.com/liamsorsby/website',
   siteLogo: '/static/images/logo.png',
-  image: '/static/images/avatar.png',
+  image: '/static/images/avatar.jpg',
   socialBanner: '/static/images/twitter-card.png',
-  email: 'address@yoursite.com',
-  github: 'https://github.com',
-  twitter: 'https://twitter.com/Twitter',
-  facebook: 'https://facebook.com',
-  youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
-  locale: 'en-US',
+  email: 'liam@sorsby.co',
+  github: 'https://github.com/liamsorsby',
+  twitter: 'https://twitter.com/liamsorsby',
+  facebook: 'https://www.facebook.com/liam.s.sorsby/',
+  youtube: '',
+  linkedin: 'https://www.linkedin.com/in/liam-sorsby/',
+  locale: 'en-GB',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
     // content security policy in the `next.config.js` file.
@@ -24,7 +24,7 @@ const siteMetadata = {
     plausibleDataDomain: '', // e.g. tailwind-nextjs-starter-blog.vercel.app
     simpleAnalytics: false, // true or false
     umamiWebsiteId: '', // e.g. 123e4567-e89b-12d3-a456-426614174000
-    googleAnalyticsId: '', // e.g. UA-000000-2 or G-XXXXXXX
+    googleAnalyticsId: 'G-6FMYRDD6HW', // e.g. UA-000000-2 or G-XXXXXXX
     posthogAnalyticsId: '', // posthog.init e.g. phc_5yXvArzvRdqtZIsHkEm3Fkkhm3d0bEYUXCaFISzqPSQ
   },
   newsletter: {
